a9bc50d09f5e6405de74b539c0ddc266540664cb
dev-nischay <nischay.mehn@gmail.com>
2025-08-26 13:09:23 +0530

    added Error Page

---
+++ amazon-cart/src/pages/Error.jsx
@@ -0,0 +1,15 @@
+export default () => {
+  return (
+    <div
+      style={{
+        fontSize: 80,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      Something went Wrong
+    </div>
+  );
+};
